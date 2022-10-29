56ab85a5dc6f4f30bbae04fe20ee91e497104e69
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-10-29 14:22:59 +0300

    Updates task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('form.login-form');
+loginForm.addEventListener("submit", loginFormProcessor);
 
+function loginFormProcessor(event) {
+    event.preventDefault();
+    console.dir(event);
+
+    if (event.target.elements.email.value ==="" || event.target.elements.password.value === "") {
+        alert("Всі поля повинні бути заповнені!");
+        return;
+    }
+
+    const loginData = {
+        a: event.target.elements.email.value,
+        b: event.target.elements.password.value,
+    }
+
+    loginForm.reset();
+    console.log(event);
+}
