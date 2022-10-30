ae9adf81f7f96b2068cdbd622f7cab9de19fc3f6
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2022-10-30 10:51:07 +0200

    Completes task 9 HW 6

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorizerBtn = document.querySelector("button.change-color")
+const colorInformer = document.querySelector("span.color")
+const body = document.querySelector("body")
+
+colorizerBtn.addEventListener("click", colorizeWindow);
+
+function colorizeWindow() {
+  const newColor = getRandomHexColor();
+  
+  console.log(colorInformer.textContent);
+  body.style.backgroundColor = newColor;
+  colorInformer.textContent = newColor.toUpperCase();
+}
